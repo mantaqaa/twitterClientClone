131fddc736e072c0048540823dcba59cd6dcb0be
Matthew <matthew.b.rodriguez@gmail.com>
2019-03-01 19:12:01 -0500

    Implement login functionality

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by Matthew Rodriguez on 3/1/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 5
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
+
+}
