1d48fec50bdb98ac61d5bb28c0264e2b34a1fc42
Parsha <mantaqaakhan@mantaqaas-macbook-pro.local>
2019-03-02 22:35:36 -0500

    twitter proj

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -14,26 +14,36 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        loginButton.layer.borderWidth = 1
-        loginButton.layer.cornerRadius = 15
+    
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+     
+        if UserDefaults.standard.bool(forKey:"userLoggedIn")==true {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+
+    }
+    
+    
+    
+    
 
     @IBAction func onTapLogin(_ sender: Any) {
         let urlString = "https://api.twitter.com/oauth/request_token"
         TwitterAPICaller.client?.login(url: urlString, success: {
             // on login success, present homeTableVC
+            
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
             self.performSegue(withIdentifier: "loginToHome", sender: self)
+            
+        
+            
         }, failure: { (Error) in
             // notify user error has occurred
             print("Error. Could not log in.")
-            // set up an alert controller
-            let title = "Error"
-            let message = "An error has occured. Unable to log-in."
-            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
         })
     }
     
+
 }
